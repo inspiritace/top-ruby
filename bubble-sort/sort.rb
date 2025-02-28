e1b41a038a63acf17c77e20416a507a26e526b6c
inspiritace <inspiritace@gmail.com>
2025-02-28 16:13:24 -0600

    fixed bubble-sort

--- bubble-sort/sort.rb
+++ bubble-sort/sort.rb
@@ -1,6 +1,3 @@
-#Build a method #bubble_sort that takes an array and returns a sorted array.
-#It must use the bubble sort methodology (using #sort would be pretty pointless, wouldn't it?).
-
 def bubble_sort(arr)
   n = arr.length
   loop do
