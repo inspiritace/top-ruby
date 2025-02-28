891f0f96c6b673b84e5534c3f9b480add61b108a
inspiritace <inspiritace@gmail.com>
2025-02-28 15:55:42 -0600

    caesar cipher final

--- caesar-cipher/cipher.rb
+++ caesar-cipher/cipher.rb
@@ -1,10 +1,3 @@
-# Implement a Caesar cipher that takes in a string and the shift factor and then outputs the modified string using a right shift
-# > caesar_cipher("What a string!", 5)
-# => "Bmfy f xywnsl!"
-# You will need to remember how to convert a string into a number.
-# Don't forget to wrap from z to a.
-# Don't forget to keep the same case.
-
 def caesar_cipher(string, shift)
   string.chars.map do |char|
     if char.match?(/[a-z]/)
